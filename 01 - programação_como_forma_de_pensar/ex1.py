d0884a22a6a48e9ec565f8e16f8ad063eff1bc38
GustavoJVvieira <gustavojvieira2004@gmail.com>
2025-07-16 17:08:46 -0300

    subindo exercicios cap 1

---
+++ 01 - programação_como_forma_de_pensar/ex1.py
@@ -0,0 +1,19 @@
+# 1 - quantos segundos existem em 42 minutos e 42 segundos ?
+
+print(f"Existem {42*60+42} segundos")
+
+# 2 - Quatas milhas existem em 10km?
+
+print(f"10km é equivalente a {10/1.61:.2f} milhas")
+
+# 3 - Se voce correr 10km em 42 minutos e 42 segundos qual será o seu o pace medio em milhas
+
+print(f"Meu pace médio será de: {(42 + 42/60) / (10 / 1.61):.2f} minutos por milha")
+
+# 4 - Qual seu ritmo medio em segundos ?
+print(f"Meu pace médio será de: {((42 + 42/60) / (10 / 1.61))*60:.2f} segundos por milha")
+
+# 5 - Qual seu ritmo por hora ?
+
+print(f"Meu pace médio será de: {((42 + 42/60) / (10 / 1.61)) / 60:.2f} horas por milha")
+
